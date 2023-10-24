543cdaab051a4e12c9f39fa872e5f21b5788e82a
akinolademilade <demiladeaduragbemi@gmail.com>
2023-10-24 20:38:51 +0100

    changed the frontend url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { readdirSync } = require("fs");
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173", "http://127.0.0.1:5173"],
+    origin: ["http://127.0.0.1:5173", "https://expense-frontend-app.netlify.app/"],
     credentials: true,
   })
 );
